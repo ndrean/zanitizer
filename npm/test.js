0f832f88b29e930becb9b2fdd59c9e0fdb0ebfcb
ndrean <nevendrean@yahoo.fr>
2026-03-06 23:21:13 +0100

    Tests npm & brew ok

--- npm/test.js
+++
@@ -1,8 +0,0 @@
-import { loadZanitize } from 'zanitize';
-
-const zan = await loadZanitize(
-  new URL('./node_modules/zanitize/zanitize.wasm', import.meta.url)
-);
-zan.init(); // default config
-
-console.log(zan.sanitizeFragment('<script>alert(1)</script><p>ok</p>'));
